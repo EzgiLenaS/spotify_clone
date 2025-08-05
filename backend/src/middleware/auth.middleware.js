3a043a629df015c8b70e1b441e0e2e512feeeaec
EzgiLenaS <ezgilena00@gmail.com>
2025-08-05 23:52:40 +0300

    just here for the green square 🟩

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -8,9 +8,17 @@
 import { clerkClient } from "@clerk/express";
 
 // Next function provides us to protect that route check user.route for example
-export const protectRoute = async ( req, res, next ) => {
+export const protectRoute = async (req, res, next) => {
     if (!req.auth.userId) {
         // The code 401 is for unauthorization
-        res.status(401).json({ message: "Unaouthorized" });
+        res.status(401).json({ message: "Unaouthorized - You must be logged in" });
+
+        return;
     }
+
+    next();
+};
+
+export const requireAdmin = async (req, res, next ) => {
+    //if (!req.auth.userId)
 };
